1956b421032802e64507a1f17e5ce18f3960a60e
guilhermão aura gay <guilhermesilvaoliveiradiass@gmail.com>
2026-09-17 10:47:11 -0300

    Add files via upload

---
+++ (22) FUNÇÕES 2/main (3).py
@@ -0,0 +1,8 @@
+def apresentar(nome, idade):
+    print('Nome:',nome,' Idade:', idade)
+def numero(n1,n2 ,n3):
+    
+    soma = (n1 + n2 + n3) / 3
+    print(f"A média de {n1}, {n2} e {n3} é: {soma:.2f} ")
+    return numero
+numero(3,6,10)
