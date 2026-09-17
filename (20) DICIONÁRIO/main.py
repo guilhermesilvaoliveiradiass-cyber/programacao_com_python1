766d9cf7ec25ab860f0f695f3d7e07df1cf0cba1
guilhermão aura gay <guilhermesilvaoliveiradiass@gmail.com>
2026-09-17 10:45:11 -0300

    Add files via upload

---
+++ (20) DICIONÁRIO/main.py
@@ -0,0 +1,7 @@
+personagens = [
+    {"nome": "Finn", "idade":17},
+    {"nome": "Finn", "idade":17},
+]
+for personagens in personagens:
+    print("nome:", personagens["nome"])
+    print("idade:", personagens["idade"])
